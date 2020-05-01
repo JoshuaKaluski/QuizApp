2ded2384421327cdc2a46999d45c738662d0c743
JoshuaKaluski <joshua@kaluski.com>
2020-05-01 16:27:50 -0400

    Implemented questions

--- app/src/main/java/com/example/quizapp/Question.kt
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -2,7 +2,7 @@ package com.example.quizapp
 
 data class Question (
     val id: Int,
-    val question: String,
+    val questionText: String,
     val image: Int,
     val optionOne: String,
     val optionTwo: String,
--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
 
@@ -11,6 +12,20 @@ class QuizQuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_questions)
 
         val questionsList = Constants.getQuestions()
-        Log.i("Questions Size", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tvProgress.text = "$currentPosition / ${progressBar.max}"
+
+        tvQuestion.text = question!!.questionText
+        ivFlag.setImageResource(question.image)
+
+        tvOptionOne.text = question.optionOne
+        tvOptionTwo.text = question.optionTwo
+        tvOptionThree.text = question.optionThree
+        tvOptionFour.text = question.optionFour
+
     }
 }
